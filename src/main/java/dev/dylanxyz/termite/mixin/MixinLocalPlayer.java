de7d3351c3f9f8022f5260d97927f59687a440de
dylanxyz <jorge.brito.xyz@gmail.com>
2025-02-16 22:50:54 -0300

    Update MixinLocalPlayer

--- src/main/java/dev/dylanxyz/termite/mixin/MixinLocalPlayer.java
+++ src/main/java/dev/dylanxyz/termite/mixin/MixinLocalPlayer.java
@@ -3,8 +3,12 @@ package dev.dylanxyz.termite.mixin;
 import com.github.leawind.thirdperson.ThirdPerson;
 import com.github.leawind.thirdperson.ThirdPersonEvents;
 import com.github.leawind.thirdperson.ThirdPersonStatus;
+import com.github.leawind.thirdperson.config.Config;
 import com.github.leawind.thirdperson.core.rotation.RotateTargetEnum;
+import com.mojang.authlib.GameProfile;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,17 +16,31 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.valkyrienskies.mod.common.entity.ShipMountingEntity;
+import org.valkyrienskies.mod.common.VSGameUtilsKt;
+import org.valkyrienskies.mod.common.entity.ShipMountedToData;
 
 @Mixin(LocalPlayer.class)
-public class MixinLocalPlayer {
+public class MixinLocalPlayer extends AbstractClientPlayer {
+    public MixinLocalPlayer(ClientLevel p_250460_, GameProfile p_249912_) {
+        super(p_250460_, p_249912_);
+    }
+
     @Inject(
         method = "startRiding",
         at = @At(value = "RETURN")
     )
     private void termite$startRiding(Entity entity, boolean p_108668_, CallbackInfoReturnable<Boolean> cir) {
-        if (cir.getReturnValue() && entity instanceof ShipMountingEntity) {
-            var config = ThirdPerson.getConfig();
+        final Minecraft minecraft = Minecraft.getInstance();
+        final ClientLevel clientLevel = minecraft.level;
+
+        if (!cir.getReturnValue() || clientLevel == null) {
+            return;
+        }
+
+        final ShipMountedToData shipMountedToData = VSGameUtilsKt.getShipMountedToData(this, null);
+
+        if (shipMountedToData != null) {
+            final Config config = ThirdPerson.getConfig();
 
             if (ThirdPersonStatus.isRenderingInThirdPerson() && config.is_mod_enabled) {
                 ThirdPerson.ENTITY_AGENT.setRotateTarget(RotateTargetEnum.CAMERA_ROTATION);
